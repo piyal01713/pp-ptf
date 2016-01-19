604033a7de32c1cd9838a4cf8fd67d6ac5f454e5
faridhub95 <farid_yid13@yahoo.com>
2016-01-19 17:22:23 +0800

    tambah 3 file baru

---
+++ app/Job-List.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<Title>Job List</Title>
+    <link rel="stylesheet" type="text/css" href="Job-List.css">
+</head>
+
+<body>
+    <div class="main">
+        <header>
+            <h1>Job List</h1>
+        </header>
+
+        <p class="createbtn">
+            <a href="create-post.php">Create New Post</a>
+        </p>
+
+        <p>
+            <table border="1" width="100%">
+                <thead>
+                    <tr>
+                        <th>Job Title</th>
+                        <th>Company</th>
+                        <th>Salary</th>
+                        <th>Date Created</th>
+                        </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td><?php echo $_POST["job_title"]; ?></td>
+                    <td><?php echo $_POST["company"]; ?></td>
+                    <td><?php echo $_POST["salary"]; ?></td>
+                    <td><?php echo $_POST["date"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td colspan="4"><hr></td>
+                    </tr>
+                </tbody>
+            </table>
+
+    <div>
+</body>
+</html>
