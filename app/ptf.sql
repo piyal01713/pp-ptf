a24257dea4d7a17e56f05e0f0907293750bee934
faridhub95 <farid_yid13@yahoo.com>
2016-02-12 15:08:29 +0800

    edited ptf.sql file

--- app/ptf.sql
+++ app/ptf.sql
@@ -26,7 +26,7 @@ CREATE TABLE `posts` (
   `work` varchar(50) DEFAULT NULL,
   `employer` varchar(50) DEFAULT NULL,
   `salary` int(11) DEFAULT NULL,
-  `location` varchar(100) DEFAULT NULL,
+  `location` varchar(2000) DEFAULT NULL,
   `scope` varchar(2000) DEFAULT NULL,
   `addinfo` varchar(2000) DEFAULT NULL,
   `jobcat` varchar(100) DEFAULT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE `posts` (
   `date_posted` date DEFAULT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES ('Mechanic','Tools Sdn.Bhd.',10,'Taman 123,\r\nJalan 1/23,\r\nTaman Area 123,\r\n52100 Kuala Lumpur.','Employee will be hired as an assistant','Starts at 10.00am to 2.00pm','Services','Kuala Lumpur','2016-02-02',1),('Janitor','Mr.Clean',20,'Area Pandan Jaya,\r\nJalan 3/2,\r\nPandan Jaya,\r\n55300 Kuala Lumpur','work by mopping floors','Starts at 9.00am - 3.00pm','Services','Kuala Lumpur','2016-02-03',2),('Waiter','Catering Sdn Bhd',30,'Taman Cheras,\r\nJalan 1/1\r\nTaman Area Cheras,\r\n55100 Kuala Lumpur','bla bla bla','start at 9.00am - 6.00pm','Services','Kuala Lumpur','2016-02-03',3);
+INSERT INTO `posts` VALUES ('Mechanics','Tools Sdn.Bhd.',10,'Taman 123,\r\nJalan 1/23,\r\nTaman Area 123,\r\n52100 Kuala Lumpur.','Employee will be hired as an assistant','Starts at 10.00am to 2.00pm','Services','Kuala Lumpur','2016-02-02',1),('Janitor','Mr.Clean',20,'Area Pandan Jaya,\r\nJalan 3/2,\r\nPandan Jaya,\r\n55300 Kuala Lumpur','work by mopping floors','Starts at 9.00am - 3.00pm','Services','Kuala Lumpur','2016-02-03',2),('Programmer','Pocketpixel',150,'jdhajhc\r\njksdhkjnknfnsfkjsnf\r\najfhdsljfhdlsjfhsdp','programming\r\nuser requirement\r\nbig data analysis\r\nsocial media research','were looking for someone whos capable and smart. and cool. and nice.','Services','Selangor','2016-02-03',6),('dsaadd','asdas',1321,'dasds','dasdas','dsadas','Services','Kuala Lumpur','2016-02-12',8);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,12 +55,13 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` int(11) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
   `email` varchar(254) DEFAULT NULL,
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
-  `date_created` date DEFAULT NULL
+  `date_created` date DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -82,4 +83,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-02-03 17:19:58
+-- Dump completed on 2016-02-12 15:07:21
