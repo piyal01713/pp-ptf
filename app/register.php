d10e8b19c0e3077647bb59c6977dbce3e39f3867
faridhub95 <farid_yid13@yahoo.com>
2016-03-14 18:00:39 +0800

    change table column & add access control to register.php

--- app/register.php
+++ app/register.php
@@ -1,61 +1,49 @@
 <?php
 include 'dbcon.php';
 session_start();
-if(isset($_SESSION['user'])){
-    	header('Location: index.php');
-} 
-if($conn){
 
-	$sql1="SELECT username,email FROM user WHERE username = '$_POST[username]' ";
-	$results1=mysqli_query($conn, $sql1);
+$sql1 = "SELECT username,email FROM user";
+$results1 = mysqli_query($conn, $sql1);
+$rows = mysqli_num_rows($results1);
+while($compare = mysqli_fetch_array($results1)){
 
-			if(mysqli_num_rows($results1) > 0){
+$compare['username'];
+$compare['email'];
 
-				while($userinfo=mysqli_fetch_array($results1)){
+if($_POST['email'] == $compare['email']){
 
-				$uservalidation = '';
-				$emailvalidation = '';
-				$confirmpwd = '';
+	$emailerror = "This Email Has Been Registered";
+	header('Location: userlogin.php')
+}elseif($_POST['username'] == $compare['username']){
 
-				$userinfo['username'];
-				$userinfo['email'];
+	$usererror = "This Username Has Been Taken";
+	header('Location: userlogin.php')
+}else{
 
 
-					if($_POST['email'] == $userinfo['email']){
+	$sql="INSERT INTO user(
+			type,
+			email,
+			username,
+			password,
+			date_created)
+		VALUES('$_POST[type]',
+			'$_POST[email]',
+			'$_POST[username]',
+			'$_POST[password]',
+			'$_POST[regdate]')";
 
-						$emailvalidation = 'This email has already been registered';
+		if(mysqli_query($conn, $sql)){
+		echo "<h1>You have sucessfully registered</h1><br>";
+		echo "<a href='userlogin.php'>Back to login page</a>";
+		}else{
+				echo "Error: ". "<br>" . $sql . "<br>" . mysqli_error($conn);
+		}
 
-					}elseif($_POST['username'] == $userinfo['username']){
-							
-							$uservalidation = 'This username already exist';
-
-					}elseif($_POST['confirmpwd'] == $_POST['password']){
-
-						$sql="INSERT INTO user(
-								type,
-								email,
-								username,
-								password,
-								date_created)
-							VALUES('$_POST[type]',
-								'$_POST[email]',
-								'$_POST[username]',
-								'$_POST[password]',
-								'$_POST[regdate]')";
-
-							if(mysqli_query($conn, $sql)){
-							echo "<h1>You have sucessfully registered</h1><br>";
-							echo "<a href='userlogin.php'>Back to login page</a>";
-							}else{
-									echo "Error: ". "<br>" . $sql . "<br>" . mysqli_error($conn);
-							}
-					}else{
-
-						$confirmpwd = 'It is not a match';
-					}
-					
-				}
-			}
 mysqli_close($conn);
+}
+
+if(isset($_SESSION['user'])){
+header('Location: index.php');
 }
 ?>
