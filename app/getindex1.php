adc44f9162120d607c67b0ff6f808759a31f39bb
faridhub95 <farid_yid13@yahoo.com>
2016-04-05 10:53:19 +0800

    update codes

--- app/getindex1.php
+++ app/getindex1.php
@@ -11,7 +11,7 @@ $sql = "SELECT type FROM user WHERE username = '$_SESSION[user]' ";
 $results = mysqli_query($conn, $sql);
 
 //check if record exist
-if(mysqli_num_rows($results) > 0
+if(mysqli_num_rows($results) > 0){
     //loop to fetch record
     while($userinfo = mysqli_fetch_array($results)){
         //set record as variable
